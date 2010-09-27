28cf64e8bd5b01310cae6e338ec058ce39a55ed1
Francois Paradis <Francois.Paradis2@videotron.ca>
2010-09-27 19:12:24 -0400

    Got MessageService in place

---
+++ Source/Mox.UI/Project/Source/ViewModel/Main/IMessageService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mox.UI
+{
+    public interface IMessageService
+    {
+    }
+}
---
+++ Source/Mox.UI/Tests/Source/ViewModel/Main/MessageServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Mox.UI
+{
+    [TestFixture]
+    public class MessageServiceTests
+    {
+        #region Tests
+
+        [Test]
+        public void Test_Construction_values()
+        {
+            Assert.IsNotNull(MessageService.Instance);
+        }
+
+        [Test]
+        public void Test_Can_Swap_instance()
+        {
+            MockRepository mockery = new MockRepository();
+            var newInstance = mockery.StrictMock<IMessageService>();
+
+            var oldInstance = MessageService.Instance;
+            Assert.AreNotEqual(newInstance, oldInstance);
+
+            using (MessageService.Use(newInstance))
+            {
+                Assert.AreEqual(newInstance, MessageService.Instance);
+            }
+            Assert.AreEqual(oldInstance, MessageService.Instance);
+        }
+
+        #endregion
+    }
+}
