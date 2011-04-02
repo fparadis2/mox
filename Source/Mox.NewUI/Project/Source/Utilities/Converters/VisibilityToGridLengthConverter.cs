09bd1f4b525b579c0bc286edd67ac3b4b1833081
Francois Paradis <Francois.Paradis2@videotron.ca>
2011-04-01 21:27:04 -0400

    Fixed chatbox space not collapsing correctly

--- Source/Mox.NewUI/Project/Source/Utilities/Converters/VisibilityToGridLengthConverter.cs
+++ Source/Mox.NewUI/Project/Source/Utilities/Converters/VisibilityToGridLengthConverter.cs
@@ -7,6 +7,25 @@ namespace Mox.UI.Converters
 {
     public class VisibilityToGridLengthConverter : IValueConverter
     {
+        #region Constructor
+
+        public VisibilityToGridLengthConverter()
+        {
+            DefaultGridLength = new GridLength(1, GridUnitType.Star);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public GridLength DefaultGridLength
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
         #region Implementation of IValueConverter
 
         public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
@@ -17,7 +36,7 @@ namespace Mox.UI.Converters
             {
                 case Visibility.Visible:
                 case Visibility.Hidden:
-                    return new GridLength(1, GridUnitType.Star);
+                    return DefaultGridLength;
 
                 case Visibility.Collapsed:
                     return new GridLength(0, GridUnitType.Pixel);
--- Source/Mox.NewUI/Project/Source/Views/Shell/Navigation/MoxWorkspaceView.xaml.cs
+++ Source/Mox.NewUI/Project/Source/Views/Shell/Navigation/MoxWorkspaceView.xaml.cs
@@ -18,6 +18,39 @@ namespace Mox.UI.Shell
 	/// </summary>
 	public partial class MoxWorkspaceView : UserControl
 	{
+        public static readonly DependencyProperty BottomViewVisibilityProperty = DependencyProperty.Register("BottomViewVisibility", typeof(Visibility), typeof(MoxWorkspaceView), new FrameworkPropertyMetadata(Visibility.Visible, (PropertyChangedCallback)OnBottomViewVisibilityChanged));
+
+        public Visibility BottomViewVisibility
+        {
+            get { return (Visibility)GetValue(BottomViewVisibilityProperty); }
+            set { SetValue(BottomViewVisibilityProperty, value); }
+        }
+
+        private static void OnBottomViewVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((MoxWorkspaceView)d).OnBottomViewVisibilityChanged((Visibility)e.NewValue);
+        }
+
+	    private GridLength m_lastBottomRowHeight = new GridLength(250, GridUnitType.Pixel);
+
+        private void OnBottomViewVisibilityChanged(Visibility visibility)
+        {
+            switch (visibility)
+            {
+                case Visibility.Visible:
+                case Visibility.Hidden:
+                    bottomRow.MinHeight = 150;
+                    bottomRow.Height = m_lastBottomRowHeight;
+                    break;
+
+                case System.Windows.Visibility.Collapsed:
+                    m_lastBottomRowHeight = bottomRow.Height;
+                    bottomRow.MinHeight = 0;
+                    bottomRow.Height = new GridLength(0, GridUnitType.Pixel);
+                    break;
+            }
+        }
+
 		public MoxWorkspaceView()
 		{
 			this.InitializeComponent();
