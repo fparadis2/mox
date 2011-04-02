64ffac768528e79288ecb18b0e5b0d6519fd3803
Francois Paradis <Francois.Paradis2@videotron.ca>
2011-04-02 14:03:24 -0400

    Reintroduced ken burns effect

---
+++ Source/Mox.NewUI/Project/Source/Controls/Panels/CardImageKenBurnsCanvas.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using Mox.Database;
+
+namespace Mox.UI
+{
+    public class CardImageKenBurnsCanvas : KenBurnsCanvas
+    {
+        private readonly IRandom m_random = Random.New();
+
+        protected override object GetNextContent(out object readyObject)
+        {
+            // Random card, then random instance
+            var database = MasterCardDatabase.Instance;
+            CardInstanceInfo randomInstance = null;
+            while (randomInstance == null)
+            {
+                CardInfo randomCard = m_random.Choose(database.Cards);
+                var instances = randomCard.Instances.Where(instance => instance.Index > 0).ToList();
+                if (instances.Count > 0)
+                {
+                    randomInstance = m_random.Choose(instances);
+                }
+            }
+
+
+            ImageKey key = ImageKey.ForCardImage(randomInstance, false);
+            
+            var image = new Image
+            {
+                Width = 480,
+                Height = 680
+            };
+
+            ImageService.SetKey(image, key);
+
+            readyObject = image;
+            return image;
+        }
+
+        protected override bool IsReady(object readyObject)
+        {
+            Image image = (Image)readyObject;
+            return image.Source != null;
+        }
+    }
+}
