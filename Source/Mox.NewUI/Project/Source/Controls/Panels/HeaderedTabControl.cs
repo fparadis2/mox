aa095ffc1a311b3ee72e6d093765ea7f63ee40b3
Francois Paradis <Francois.Paradis2@videotron.ca>
2011-04-17 17:42:23 -0400

    Headered tab control

---
+++ Source/Mox.NewUI/Project/Source/Controls/Panels/HeaderedTabControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Mox.UI
+{
+    /// <summary>
+    /// The purpose of this control is to be able to restyle the top part of a TabControl.
+    /// </summary>
+    public class HeaderedTabControl : TabControl
+    {
+        #region Dependency Properties
+
+        public static DependencyProperty HeaderTemplateProperty = DependencyProperty.Register("HeaderTemplate", typeof(ControlTemplate), typeof(HeaderedTabControl));
+
+        public ControlTemplate HeaderTemplate
+        {
+            get { return (ControlTemplate)GetValue(HeaderTemplateProperty); }
+            set { SetValue(HeaderTemplateProperty, value); }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        static HeaderedTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HeaderedTabControl), new FrameworkPropertyMetadata(typeof(HeaderedTabControl)));
+        }
+
+        #endregion
+    }
+}
