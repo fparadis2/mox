7b267f765592822eff8b0a8b309c513d707e9fa1
Francois Paradis <Francois.Paradis2@videotron.ca>
2011-09-21 21:04:54 -0400

    Forget files

---
+++ Source/Mox.NewUI/Project/Source/Views/Browser/Decks/ImportDeckWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace Mox.UI.Browser
+{
+	/// <summary>
+	/// Interaction logic for ImportDeckWindow.xaml
+	/// </summary>
+	public partial class ImportDeckWindow : Window
+    {
+        #region Constructor
+
+        public ImportDeckWindow()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+        }
+
+        #endregion
+
+        #region Properties
+
+        public new ImportDeckViewModel DataContext
+        {
+            get { return (ImportDeckViewModel)base.DataContext; }
+            set { base.DataContext = value; }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        #endregion
+	}
+}
