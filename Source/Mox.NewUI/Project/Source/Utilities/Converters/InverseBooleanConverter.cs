f3d50a12afb160fe31eb784da3603bca090c4d6c
Francois Paradis <Francois.Paradis2@videotron.ca>
2011-10-04 21:20:33 -0400

    DropDownButton

---
+++ Source/Mox.NewUI/Project/Source/Utilities/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mox.UI.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+
+        #endregion
+    }
+}
