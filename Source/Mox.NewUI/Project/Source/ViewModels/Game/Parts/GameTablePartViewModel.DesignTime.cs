fb455fef081ad4e6456d6b9ed17f200e92b5d80c
Francois Paradis <Francois.Paradis2@videotron.ca>
2012-05-04 21:03:51 -0400

    Battlefield control

---
+++ Source/Mox.NewUI/Project/Source/ViewModels/Game/Parts/GameTablePartViewModel.DesignTime.cs
@@ -0,0 +1,10 @@
+﻿namespace Mox.UI.Game
+{
+    public class GameTablePartViewModel_DesignTime : GameTablePartViewModel
+    {
+        public GameTablePartViewModel_DesignTime()
+            : base(new GameViewModel_DesignTime())
+        {
+        }
+    }
+}
