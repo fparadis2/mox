c4c21da1d652c1c927a0db8a9ed47f04340afa72
Francois Paradis <fparadis@gmail.com>
2014-03-09 10:17:27 -0400

    Redid +/- buttons in deck editor

---
+++ Source/Mox.NewUI/Project/Source/Controls/Common/SegoeSymbolButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Mox.UI
+{
+    public class SegoeSymbolButton : Button
+    {
+        static SegoeSymbolButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SegoeSymbolButton), new FrameworkPropertyMetadata(typeof(SegoeSymbolButton)));
+        }
+
+        public static readonly DependencyProperty SymbolTextProperty = DependencyProperty.Register(
+            "SymbolText", typeof (string), typeof (SegoeSymbolButton), new PropertyMetadata(default(string)));
+
+        public string SymbolText
+        {
+            get { return (string) GetValue(SymbolTextProperty); }
+            set { SetValue(SymbolTextProperty, value); }
+        }
+    }
+}
