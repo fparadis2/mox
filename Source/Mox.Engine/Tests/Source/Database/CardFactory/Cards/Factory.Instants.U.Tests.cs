27bf27a1c3cc0ee03e8ec19acf0d00f252cd4cee
Francois Paradis <fparadis@gmail.com>
2018-02-24 13:28:07 -0500

    Added tests for counterspell

---
+++ Source/Mox.Engine/Tests/Source/Database/CardFactory/Cards/Factory.Instants.U.Tests.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) François Paradis
+// This file is part of Mox, a card game simulator.
+// 
+// Mox is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+// 
+// Mox is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Mox.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Linq;
+using Mox.Rules;
+using NUnit.Framework;
+using Mox.Abilities;
+using Mox.Events;
+
+namespace Mox.Database.Sets
+{
+    [TestFixture]
+    public class FactoryInstantsBlueTests : BaseFactoryTests
+    {
+        #region Counterspell
+
+        [Test]
+        public void Test_Counterspell()
+        {
+            Card counterspell = InitializeCard("Counterspell");
+            Assert.AreEqual(Type.Instant, counterspell.Type);
+
+            PlayCardAbility playAbility = GetPlayCardAbility(counterspell);
+            Assert.IsFalse(CanPlay(m_playerA, playAbility)); // No spell to counter
+
+            Card creatureCard = InitializeCard("Ornithopter");
+            Play(m_playerA, GetPlayCardAbility(creatureCard));
+
+            Assert.AreEqual(1, m_game.SpellStack2.Count);
+
+            Assert.IsTrue(CanPlay(m_playerA, playAbility));
+            Expect_Target(m_playerA, PermanentFilter.AnyCreature, m_game.SpellStack2.Last());
+            Expect_PayManaCost(m_playerA, "UU");
+            PlayAndResolve(m_playerA, playAbility);
+
+            Assert.AreEqual(0, m_game.SpellStack2.Count);
+            Assert.AreEqual(m_game.Zones.Graveyard, creatureCard.Zone);
+        }
+
+        [Test]
+        public void Test_Counterspell_cannot_counter_activated_abilities()
+        {
+            Card counterspell = InitializeCard("Counterspell");
+            Assert.AreEqual(Type.Instant, counterspell.Type);
+
+            PlayCardAbility playAbility = GetPlayCardAbility(counterspell);
+            Assert.IsFalse(CanPlay(m_playerA, playAbility)); // No spell to counter
+
+            Card creatureCard = InitializeCard("Prodigal Sorcerer");
+            creatureCard.Zone = m_game.Zones.Battlefield;
+            Play(m_playerA, creatureCard.Abilities.OfType<ActivatedAbility>().Single());
+
+            Assert.AreEqual(1, m_game.SpellStack2.Count);
+            Assert.IsFalse(CanPlay(m_playerA, playAbility));
+        }
+
+        #endregion
+    }
+}
